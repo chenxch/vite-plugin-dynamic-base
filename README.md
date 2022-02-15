e1002a173f75cb1ebeaae04bf1c923a224490c55
Menci <huanghaorui301@gmail.com>
2022-02-16 05:17:50 +0800

    Support complex replacement with SWC AST traversal

--- README.md
+++ README.md
@@ -2,17 +2,21 @@
 
 [![NPM version](https://img.shields.io/npm/v/vite-plugin-dynamic-base?color=a1b858&label=)](https://www.npmjs.com/package/vite-plugin-dynamic-base)
 
-- 🦾 Resolve all resource files dynamic publicPath, like Webpack's \_\_webpack_public_path__.
+- 🦾 Resolve all resource files dynamic publicPath, like Webpack's `__webpack_public_path__`.
 
 ## Installation
 
 ```bash
+# NPM
 npm i vite-plugin-dynamic-base -D
+# Yarn
+yarn add vite-plugin-dynamic-base -D
 ```
+
 ## Build Mode
 
 - [x] es
-- [ ] system
+- [x] system
 
 ## Usage
 
@@ -21,6 +25,8 @@ npm i vite-plugin-dynamic-base -D
 import { dynamicBase } from 'vite-plugin-dynamic-base'
 
 export default defineConfig({
+  // Set `config.base` to a unique placeholder when building (but NOT previewing)
+  base: process.env.NODE_ENV === 'production' ? '/__vite_base__/' : '/',
   plugins: [
     dynamicBase({ /* options */ }),
   ],
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,81 @@
 import type { Plugin, IndexHtmlTransformResult } from 'vite'
 import { parse } from 'node-html-parser'
+import { BinaryExpression, Expression, ExpressionStatement, StringLiteral } from '@swc/core';
+import * as swc from '@swc/core';
+import { Visitor } from '@swc/core/Visitor';
+
 import type { Options } from '../index'
+
+class PlaceholderReplacer extends Visitor {
+  private readonly placeholder: string;
+  private readonly expression: Expression;
+
+  constructor(placeholder: string, expression: string) {
+    super();
+    this.placeholder = placeholder;
+    this.expression = PlaceholderReplacer.parseExpression(expression);
+  }
+
+  static parseExpression(expression: string) {
+    return (swc.parseSync(expression).body[0] as ExpressionStatement).expression;
+  }
+
+  // @ts-ignore We changed expression type
+  visitStringLiteral(node: StringLiteral): Expression {
+    const stringParts = node.value.split(this.placeholder);
+    if (stringParts.length === 1) return node;
+
+    const createStringExpression = (str: string): StringLiteral => ({
+      type: 'StringLiteral',
+      span: { start: 0, end: 0, ctxt: 0 },
+      value: str,
+      // @ts-ignore Typing is wrong: hasEscape => has_escape
+      hasEscape: true,
+      kind: { type: 'normal', containsQuote: true }
+    });
+
+    let subExpressions: Expression[] = Array(stringParts.length * 2 - 1);
+    for (let i = 0; i < stringParts.length; i++) {
+      subExpressions[i * 2] = createStringExpression(stringParts[i]);
+      if (i !== stringParts.length - 1) {
+        subExpressions[i * 2 + 1] = this.expression;
+      }
+    }
+
+    subExpressions = subExpressions.filter(expr => expr.type !== 'StringLiteral' || expr.value !== '');
+
+    if (subExpressions.length === 1) return subExpressions[0];
+
+    const createAddExpression = (left: Expression): BinaryExpression => ({
+      type: 'BinaryExpression',
+      span: { start: 0, end: 0, ctxt: 0 },
+      operator: '+',
+      left: left,
+      right: null
+    });
+
+    let rootExpression: Expression;
+    let previousExpression: BinaryExpression;
+    for (let i = 0; i < subExpressions.length; i++) {
+      const currentSubExpression = subExpressions[i];
+      if (i === 0) {
+        previousExpression = rootExpression = createAddExpression(currentSubExpression);
+      } else if (i === subExpressions.length - 1) {
+        previousExpression.right = currentSubExpression;
+      } else {
+        previousExpression.right = createAddExpression(currentSubExpression);
+        previousExpression = previousExpression.right;
+      }
+    }
+
+    return rootExpression;
+  }
+}
 
 export function dynamicBase(options?: Options): Plugin {
   const defaultOptions:Options ={
     publicPath: 'window.__dynamic_base__',
-    transformIndexHtml: false // maybe dfault true
+    transformIndexHtml: false // maybe default true
   } 
   
   const { 
@@ -13,9 +83,9 @@ export function dynamicBase(options?: Options): Plugin {
     transformIndexHtml
   } = {...defaultOptions ,...(options || {})}
 
-  const preloadHelperId = 'vite/preload-helper'
   let assetsDir = 'assets'
   let base = '/'
+  let minify = true
 
   return {
     name: 'vite-plugin-dynamic-base',
@@ -24,27 +94,29 @@ export function dynamicBase(options?: Options): Plugin {
     configResolved(resolvedConfig) {
       assetsDir = resolvedConfig.build.assetsDir
       base = resolvedConfig.base
+      minify = !!resolvedConfig.build.minify
     },
-    transform(code, id) {
-      if (id === preloadHelperId) {
-        code = code.replace(/(\${base})/g, `\${${publicPath}}$1`)
-        return {
-          code
-        }
-      }
-    },
-    generateBundle({ format }, bundle) {
-      if (format !== 'es') {
+    async generateBundle({ format }, bundle) {
+      if (format !== 'es' && format !== 'system') {
         return
       }
-      const assetsMarker = `${base}${assetsDir}/`
-      const assetsMarkerRE = new RegExp(`("${assetsMarker}*.*.*")`, 'g')
-      for (const file in bundle) {
-        const chunk = bundle[file]
-        if (chunk.type === 'chunk' && chunk.code.indexOf(assetsMarker) > -1) {
-          chunk.code = chunk.code.replace(assetsMarkerRE, `${publicPath}+$1`)
+
+      // To replace the placeholder in emitted asset files (e.g. .css)
+      const assetsRE = new RegExp(`${base}${assetsDir}/`, 'g');
+
+      await Promise.all(Object.entries(bundle).map(async ([, chunk]) => {
+        if (chunk.type === 'chunk' && chunk.code.indexOf(base) > -1) {
+          // For modern build, replace the preload URLs
+          // For legacy build, replace the import URLs and URLs in inlined CSS
+          const ast = await swc.parse(chunk.code);
+          const replacer = new PlaceholderReplacer(base, publicPath);
+          replacer.visitModule(ast);
+          chunk.code = (await swc.print(ast, { minify })).code;
+        } else if (chunk.type === 'asset' && typeof chunk.source === "string" && chunk.fileName.endsWith(".css")) {
+          // Emitted asset files for modern build, just let them use relative paths
+          chunk.source = chunk.source.replace(assetsRE, "");
         }
-      }
+      }));
     },
     transformIndexHtml: {
       enforce: 'post',
