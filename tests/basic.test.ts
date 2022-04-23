9a764f32f549fd39663042411bfb248abb49d3cc
周金富 <zhoujinfu@bytedance.com>
2022-04-23 12:27:23 +0800

    chore: setup test with vitest

---
+++ tests/basic.test.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import { dynamicBase as createPlugin } from '..'
+
+describe('setup ok', () => {
+  it('works', () => {
+    expect(defineConfig({
+      plugins: [createPlugin()],
+    })).toMatchInlineSnapshot(`
+      {
+        "plugins": [
+          {
+            "apply": "build",
+            "configResolved": [Function],
+            "enforce": "post",
+            "generateBundle": [Function],
+            "name": "vite-plugin-dynamic-base",
+            "transform": [Function],
+            "transformIndexHtml": {
+              "enforce": "post",
+              "transform": [Function],
+            },
+          },
+        ],
+      }
+    `)
+  })
+})
